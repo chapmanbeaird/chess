12cc391ea43ec58891352fe6714e56a483f3b138
Chapman Beaird <chapman.beaird@byu.edu>
2024-03-06 14:30:57 -0700

    sql auth and game dao

--- server/src/main/java/dataAccess/mysqlAuthDAO.java
+++ server/src/main/java/dataAccess/mysqlAuthDAO.java
@@ -5,28 +5,78 @@ import model.AuthData;
 
 public class mysqlAuthDAO {
 
+    private final static String CREATE_AUTH = "INSERT INTO auth_tokens (authToken, username) VALUES (?, ?)";
+    private final static String GET_AUTH = "SELECT * FROM auth_tokens WHERE authToken = ?";
+    private final static String DELETE_AUTH = "DELETE FROM auth_tokens WHERE authToken = ?";
+    private final static String CLEAR_ALL_AUTH = "DELETE FROM auth_tokens";
 
 
     // Method to create a new Auth Token
     public void createAuthToken(AuthData authToken) throws DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(CREATE_AUTH)) {
+
+            stmt.setString(1, authToken.authToken());
+            stmt.setString(2, authToken.username());
+            stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new DataAccessException("Error encountered while inserting new auth token into the database", e);
+        }
     }
 
     // Method to retrieve an Auth Token by token
     public AuthData getAuthToken(String token) throws DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(GET_AUTH)) {
 
+            stmt.setString(1, token);
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                String authToken = rs.getString("authToken");
+                String username = rs.getString("username");
+                return new AuthData(authToken, username);
+            }
+            return null;
+
+        } catch (SQLException e) {
+            throw new DataAccessException("Error encountered while retrieving auth token", e);
+        }
     }
 
     // Method to get username by token
     public String getUsername(String token) throws DataAccessException {
-
+        AuthData authData = getAuthToken(token);
+        return (authData != null) ? authData.username() : null;
     }
 
     // Method to delete an Auth Token by token
     public void deleteAuthToken(String token) throws DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(DELETE_AUTH)) {
+
+            stmt.setString(1, token);
+            int affectedRows = stmt.executeUpdate();
+
+            if (affectedRows == 0) {
+                throw new DataAccessException("Auth token deletion failed: it does not exist", null);
+            }
+
+        } catch (SQLException e) {
+            throw new DataAccessException("Error encountered while deleting auth token", e);
+        }
     }
 
     // Method to clear all Auth Tokens
     public void clearAuthTokens() throws DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(CLEAR_ALL_AUTH)) {
 
+            stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new DataAccessException("Error encountered while clearing auth toekns", e);
+        }
     }
 }
--- server/src/main/java/dataAccess/mysqlGameDAO.java
+++ server/src/main/java/dataAccess/mysqlGameDAO.java
@@ -1,8 +1,12 @@
 package dataAccess;
 
+import chess.ChessGame;
 import model.GameData;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
+import com.google.gson.Gson;
+
 
 public class mysqlGameDAO {
 
@@ -10,24 +14,97 @@ public class mysqlGameDAO {
     private final static String GET_GAME = "SELECT * FROM games WHERE gameID = ?";
     private final static String LIST_GAMES = "SELECT * FROM games";
     private final static String UPDATE_GAME = "UPDATE games SET ... WHERE gameID = ?";
+    private final static String CLEAR_GAMES = "DELETE FROM games";
+    private Gson gson = new Gson();
+
+
 
     public void createGame(GameData game) throws DataAccessException {
         // Use SQL INSERT statement to add game
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(CREATE_GAME, Statement.RETURN_GENERATED_KEYS)) {
+
+            stmt.setString(1, game.whiteUsername());
+            stmt.setString(2, game.blackUsername());
+            stmt.setString(3, game.gameName());
+            stmt.setString(4, gson.toJson(game.game())); // converts ChessGame object to json
+            stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new DataAccessException("Error encountered while inserting new game into the database", e);
+        }
     }
 
     public GameData getGame(int gameId) throws DataAccessException {
         // Use SQL SELECT statement to get game
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(GET_GAME)) {
+
+            stmt.setInt(1, gameId);
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                int id = rs.getInt("gameID");
+                String whiteUsername = rs.getString("whiteUsername");
+                String blackUsername = rs.getString("blackUsername");
+                String gameName = rs.getString("gameName");
+                ChessGame game = gson.fromJson(rs.getString("game"), ChessGame.class);
+                return new GameData(id, whiteUsername, blackUsername, gameName, game);
+            } else {
+                return null;
+            }
+
+        } catch (SQLException e) {
+            throw new DataAccessException("Error encountered while retrieving game", e);
+        }
     }
 
-    public List<GameData> listGames(){
+    public List<GameData> listGames() throws DataAccessException{
         // Use SQL SELECT statement to list games
+        List<GameData> games = new ArrayList<>();
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(LIST_GAMES);
+             ResultSet rs = stmt.executeQuery()) {
+
+            while (rs.next()) {
+                int id = rs.getInt("gameID");
+                String whiteUsername = rs.getString("whiteUsername");
+                String blackUsername = rs.getString("blackUsername");
+                String gameName = rs.getString("gameName");
+                ChessGame game = gson.fromJson(rs.getString("game"), ChessGame.class);
+                games.add(new GameData(id, whiteUsername, blackUsername, gameName, game));
+            }
+
+        } catch (SQLException e) {
+            throw new DataAccessException("Error encountered while listing games", e);
+        }
+        return games;
     }
 
     public void updateGame(int gameId, GameData updatedGame) throws DataAccessException {
         // Use SQL UPDATE statement to update game
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(UPDATE_GAME)) {
+
+            stmt.setString(1, updatedGame.whiteUsername());
+            stmt.setString(2, updatedGame.blackUsername());
+            stmt.setString(3, updatedGame.gameName());
+            stmt.setString(4, gson.toJson(updatedGame.game())); //coverts chessgame to json
+            stmt.setInt(5, gameId);
+            stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new DataAccessException("Error encountered while updating game", e);
+        }
     }
 
-    public void clearGames(){
+    public void clearGames() throws DataAccessException{
         // Use SQL DELETE statement to clear games
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(CLEAR_GAMES)) {
+                stmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException("Error encountered while clearing games", e);
+        }
     }
 }
